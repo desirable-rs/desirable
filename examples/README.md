fef04659b99a51e170cda9253411e169daf7d995
Mankong Zhao <siwilizhao@gmail.com>
2024-05-25 20:03:58 +0800

    update fs

--- examples/README.md
+++ examples/README.md
@@ -5,3 +5,8 @@
 ```sh
 ENV_NAME=dev
 ```
+
+```sh
+cd examples
+cargo run -p example-hello dev
+```
--- src/fs.rs
+++ src/fs.rs
@@ -3,9 +3,7 @@ use crate::Request;
 use crate::Result;
 use bytes::Bytes;
 use http_body_util::Full;
-use std::path::Path;
 use std::path::PathBuf;
-use tracing::info;
 pub struct ServeFile {
   path: PathBuf,
 }
@@ -37,60 +35,9 @@ impl Endpoint for ServeDir {
   async fn call(&self, req: Request) -> Result {
     let file = req.param::<String>("file")?;
     let dir = self.dir.clone();
-    let ext = Path::new(&file).extension();
     let file = PathBuf::from(format!("{}/{}", dir.to_string_lossy(), file));
     let body = tokio::fs::read(file).await?;
-    info!("ext {:?}", ext);
-    let mut response = hyper::Response::new(Full::new(Bytes::from(body)));
-    if let Some(ext) = ext {
-      let ext = ext.to_string_lossy();
-      if ext.contains(&"js") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::TEXT_JAVASCRIPT.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"css") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::TEXT_CSS.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"png") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_PNG.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"png") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_PNG.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"svg") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_SVG.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"jpeg") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_JPEG.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"gif") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_GIF.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"bmp") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::IMAGE_BMP.as_ref().parse().unwrap(),
-        );
-      } else if ext.contains(&"mp4") {
-        response.headers_mut().insert(
-          hyper::header::CONTENT_TYPE,
-          mime::MP4.as_ref().parse().unwrap(),
-        );
-      }
-    }
+    let response = hyper::Response::new(Full::new(Bytes::from(body)));
     Ok(response.into())
   }
 }
